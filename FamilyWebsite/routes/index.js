96441bf5732cae6ff2f3d4125b0b46f2567a7b2d
abhijeetrana318@gmail.com <abhijeetrana318@gmail.com>
2020-06-11 17:21:27 -0400

    Add project files.

---
+++ FamilyWebsite/routes/index.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Welcome to my family info page' });
+});
+
+router.get('/abhijeet', function (req, res) {
+    res.render('abhijeet', { title: 'Here is the Abhijeet\'s info' });
+});
+
+router.get('/kulwinder', function (req, res) {
+    res.render('kulwinder', { title: 'Here is the Kulwinder\'s info' });
+});
+
+router.get('/veena', function (req, res) {
+    res.render('veena', { title: 'Here is the Veena\'s info' });
+});
+
+module.exports = router;
---
+++ FamilyWebsite/routes/users.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    var potentialNames = ['Jacob', 'Bob', 'Mike'];
+    var randomName = potentialNames[Math.floor(Math.random() * potentialNames.length)];
+    res.render('users', { 'name': randomName });
+});
+
+module.exports = router;
